d2eddf2a55c5a8dedc8986f3cc3f2b255ea0fb54
atlomak <andrzej.tlomak01@gmail.com>
2024-05-22 00:03:44 +0200

    refactor: move cuda error handling macro to header

--- project/cuda_rho_pollard/src/main.cu
+++ project/cuda_rho_pollard/src/main.cu
@@ -5,17 +5,6 @@
 #include "main.cuh"
 #include <iostream>
 
-#define cudaCheckError(ans) { gpuAssert((ans), __FILE__, __LINE__); }
-
-inline void gpuAssert(cudaError_t code, const char *file, int line, bool abort = true)
-{
-    if (code != cudaSuccess)
-    {
-        fprintf(stderr, "GPUassert: %s %s %d\n", cudaGetErrorString(code), file, line);
-        if (abort)
-            exit(code);
-    }
-}
 
 __device__ __host__ int64_t modAdd(int64_t a, int64_t b, int64_t mod)
 {
--- project/cuda_rho_pollard/src/main.cuh
+++ project/cuda_rho_pollard/src/main.cuh
@@ -4,8 +4,21 @@
 
 #ifndef MAIN_CUH
 #define MAIN_CUH
+#include <cstdio>
 
 #define P 7
+
+#define cudaCheckError(ans) { gpuAssert((ans), __FILE__, __LINE__); }
+
+inline void gpuAssert(cudaError_t code, const char* file, int line, bool abort = true)
+{
+    if (code != cudaSuccess)
+    {
+        fprintf(stderr, "GPUassert: %s %s %d\n", cudaGetErrorString(code), file, line);
+        if (abort)
+            exit(code);
+    }
+}
 
 typedef struct
 {
