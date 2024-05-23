ad3835c7695ffcee1a668c942f7d2eb875d78b0b
atlomak <andrzej.tlomak01@gmail.com>
2024-05-23 11:39:13 +0200

    refactor: ecc ops moved to new file to declutter main

--- project/cuda_rho_pollard/src/main.cuh
+++ project/cuda_rho_pollard/src/main.cuh
@@ -20,16 +20,5 @@ inline void gpuAssert(cudaError_t code, const char* file, int line, bool abort =
     }
 }
 
-typedef struct
-{
-    int64_t x;
-    int64_t y;
-} ECC_point;
-
-__device__ __host__ ECC_point add_points(const ECC_point* P1, const ECC_point* P2, int64_t Pmod);
-
-__device__ __host__ ECC_point mul_point(ECC_point P1, int64_t a, int64_t Pmod);
-
-__global__ void test_kernel_add(ECC_point* dev_a, ECC_point* dev_b, ECC_point* dev_result, int64_t mod);
 
 #endif //MAIN_CUH
--- project/cuda_rho_pollard/tests/test_ECCp-20.cu
+++ project/cuda_rho_pollard/tests/test_ECCp-20.cu
@@ -1,5 +1,6 @@
 #include <catch2/catch_test_macros.hpp>
 #include "main.cuh"
+#include "ecc.cuh"
 
 #define FIELD_ORDER 0xD3915
 #define CURVE_A_PARAM 0xb44bc
