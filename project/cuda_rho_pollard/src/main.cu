e63c532d166b296696e75c439fb14f2c904b58a7
atlomak <andrzej.tlomak01@gmail.com>
2024-06-22 19:56:51 +0200

    wip: start implementing 79bit rhop

--- project/cuda_rho_pollard/src/main.cu
+++ project/cuda_rho_pollard/src/main.cu
@@ -2,141 +2,33 @@
 // Created by atlomak on 05.05.24.
 //
 
-#include "main.cuh"
-#include "ecc32.cuh"
-#define THREADS_PER_BLOCK 10
-#define BLOCKS 1
 
+#include <gmp.h>
+#include <cgbn/cgbn.h>
 
-__device__ bool dist_point(int64_t num)
+#define TPI 4
+#define INSTANCES 1
+
+typedef cgbn_context_t<TPI> context_t;
+typedef cgbn_env_t<context_t, 128> env_t;
+
+typedef struct
 {
-    return (num & 0xFF) == 0;
-}
+    cgbn_mem_t<128> x;
+    cgbn_mem_t<128> y;
+} host_ECC_128_point;
 
-__global__ void rho_pollard(TRIPLE *starting_points, TRIPLE *results, ECDLP_params *params)
+typedef struct
 {
-    TRIPLE current_t = starting_points[threadIdx.x];
-    while (1)
-    {
-        if (current_t.X.x % 3 == 0)
-        {
-            printf("First if; a: %ld b: %ld x: %ld y: %ld\n", current_t.a, current_t.b, current_t.X.x, current_t.X.y);
-            current_t.X = add_points(&current_t.X, &params->Q, params->mod);
-            current_t.b = current_t.b + 1 % params->n;
-        }
-        else if (current_t.X.x % 3 == 1)
-        {
-            printf("Second if; a: %ld b: %ld x: %ld y: %ld\n", current_t.a, current_t.b, current_t.X.x, current_t.X.y);
-            current_t.X = mul_point(current_t.X, params->a_param, params->mod);
-            current_t.a = current_t.a * 2 % params->n;
-            current_t.b = current_t.b * 2 % params->n;
-        }
-        else
-        {
-            printf("Third if; a: %ld b: %ld x: %ld y: %ld\n", current_t.a, current_t.b, current_t.X.x, current_t.X.y);
-            current_t.X = add_points(&current_t.X, &params->P, params->mod);
-            current_t.a = current_t.a + 1 % params->n;
-        }
-        if (dist_point(current_t.X.x))
-        {
-            printf("Dist found; a: %ld b: %ld x: %ld y: %ld\n", current_t.a, current_t.b, current_t.X.x, current_t.X.y);
-            current_t.X = add_points(&current_t.X, &params->P, params->mod);
-            results[blockIdx.x + threadIdx.x] = current_t;
-            break;
-        }
-    }
+    env_t::cgbn_t x;
+    env_t::cgbn_t y;
+} dev_ECC_128_point;
+
+__device__ dev_ECC_128_point add_points(const dev_ECC_128_point *P1, const dev_ECC_128_point *P2, int64_t Pmod, env_t bn_env);
+
+__device__ dev_ECC_128_point add_points(const dev_ECC_128_point *P1, const dev_ECC_128_point *P2, int64_t Pmod, env_t bn_env)
+{    
+    env_t::cgbn_t r;
+
+    return dev_ECC_128_point{};
 }
-
-#define FIELD_ORDER 0xD3915
-#define A_PARAM 738492
-
-#define COMPUTE_SIZE THREADS_PER_BLOCK*BLOCKS
-
-#ifndef UNIT_TESTING
-int main()
-{
-
-    TRIPLE results[COMPUTE_SIZE];
-    TRIPLE starting_points[COMPUTE_SIZE];
-
-    ECC_point P{
-        863000,
-        535241,
-    };
-
-    ECC_point Q{
-        285780,
-        326638,
-    };
-
-    for (int i = 0; i < COMPUTE_SIZE; i++)
-    {
-        if (i % 2 == 0)
-        {
-            starting_points[i] = TRIPLE{
-                372092,
-                486761,
-                ECC_point{
-                    527934,
-                    865445
-                }
-            };
-        }
-        else
-        {
-            starting_points[i] = TRIPLE{
-                21,
-                902,
-                ECC_point{
-                    689804,
-                    338852
-                }
-            };
-        }
-    }
-
-    ECDLP_params params{
-        A_PARAM,
-        FIELD_ORDER,
-        866721,
-        P,
-        Q
-    };
-
-    cudaEvent_t start, stop;
-    cudaCheckError(cudaEventCreate(&start))
-    cudaCheckError(cudaEventCreate(&stop))
-    cudaCheckError(cudaEventRecord(start, 0))
-
-    TRIPLE *dev_starting_points, *dev_result;
-    ECDLP_params *dev_params;
-
-    cudaCheckError(cudaMalloc((void**)&dev_starting_points, sizeof(TRIPLE)*COMPUTE_SIZE))
-    cudaCheckError(cudaMalloc((void**)&dev_result, sizeof(TRIPLE)*COMPUTE_SIZE))
-    cudaCheckError(cudaMalloc((void**)&dev_params, sizeof(ECDLP_params)))
-
-    cudaCheckError(cudaMemcpy(dev_starting_points, starting_points, sizeof(TRIPLE)*COMPUTE_SIZE, cudaMemcpyHostToDevice))
-    cudaCheckError(cudaMemcpy(dev_params, &params, sizeof(ECDLP_params), cudaMemcpyHostToDevice))
-
-    rho_pollard<<<BLOCKS, THREADS_PER_BLOCK>>>(dev_starting_points, dev_result, dev_params);
-
-    cudaCheckError(cudaMemcpy(results, dev_result, sizeof(TRIPLE), cudaMemcpyDeviceToHost))
-
-    cudaCheckError(cudaEventRecord(stop, 0));
-    cudaCheckError(cudaEventSynchronize(stop));
-    float elapsedTime;
-    cudaCheckError(cudaEventElapsedTime(&elapsedTime, start, stop))
-
-    printf("Time to add: %3.1f\n", elapsedTime);
-
-    cudaEventDestroy(start);
-    cudaEventDestroy(stop);
-
-    cudaFree(dev_params);
-    cudaFree(dev_starting_points);
-    cudaFree(dev_result);
-
-    printf("Result: %ld : %ld", results->X.x, results->X.y);
-}
-
-#endif
--- project/cuda_rho_pollard/src/main.cuh
+++ project/cuda_rho_pollard/src/main.cuh
@@ -4,36 +4,6 @@
 
 #ifndef MAIN_CUH
 #define MAIN_CUH
-#include <cstdio>
-#include "ecc32.cuh"
-
-typedef struct
-{
-    int64_t a_param;
-    int64_t mod; // Prime field characteristics
-    int64_t n; //  Order of P
-    ECC_point P;
-    ECC_point Q;
-} ECDLP_params;
-
-typedef struct
-{
-    int64_t a;
-    int64_t b;
-    ECC_point X;
-} TRIPLE;
-
-#define cudaCheckError(ans) { gpuAssert((ans), __FILE__, __LINE__); }
-
-inline void gpuAssert(cudaError_t code, const char* file, int line, bool abort = true)
-{
-    if (code != cudaSuccess)
-    {
-        fprintf(stderr, "GPUassert: %s %s %d\n", cudaGetErrorString(code), file, line);
-        if (abort)
-            exit(code);
-    }
-}
 
 
 #endif //MAIN_CUH
