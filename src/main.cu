ecabc5b93c261f7acde2c8b5aa54b78d3415d3c1
atlomak <andrzej.tlomak01@gmail.com>
2024-07-26 01:49:39 +0200

    feat: double a point

--- src/main.cu
+++ src/main.cu
@@ -37,6 +37,7 @@ __device__ dev_ECC_192_point add_points(env192_t bn_env, const dev_ECC_192_point
     env192_t::cgbn_t s, s_sq, x3, y3, t3;
 
     cgbn_mont_mul(bn_env, s, t1, t2, Pmod, np0); // s = (y1-y2)/(x1-x2) mod Pmod // tested
+
     cgbn_mont_sqr(bn_env, s_sq, s, Pmod, np0); // s^2 mod Pmod // tested
 
     cgbn_add(bn_env, t3, x1, x2); // x1 + x2
@@ -64,22 +65,78 @@ __device__ dev_ECC_192_point add_points(env192_t bn_env, const dev_ECC_192_point
     cgbn_mont2bn(bn_env, x3, x3, Pmod, np0);
     cgbn_mont2bn(bn_env, y3, y3, Pmod, np0);
 
-    // cgbn_sub(bn_env, x3, s_sq, temp);
+    // cgbn_sub(bn_env, x3, s_sq, t1);
 
     return dev_ECC_192_point{x3, y3};
 }
 
-__device__ dev_ECC_192_point point_mul(env192_t bn_env, const dev_ECC_192_point &P, const env192_t::cgbn_t &k, const env192_t::cgbn_t &Pmod)
-{
-    dev_ECC_192_point R = P;
-    dev_ECC_192_point Q = P;
+__device__ dev_ECC_192_point double_point(env192_t &bn_env, dev_ECC_192_point &R, const dev_ECC_192_point &P, const env192_t::cgbn_t &Pmod, env192_t::cgbn_t a) {
 
-    for (int i = 1; i < 192; i++)
+    env192_t::cgbn_t x, y, s, t1, t_three;
+    uint32_t np0;
+
+    // Check if the point is at infinity or if y-coordinate is zero
+    // if (cgbn_compare_ui32(bn_env, P.x, 0) == 0 && cgbn_compare_ui32(bn_env, P.y, 0) == 0) {
+    //     cgbn_set_ui32(bn_env, R.x, 0);
+    //     cgbn_set_ui32(bn_env, R.y, 0);
+    //     return;
+    // }
+    // if (cgbn_compare_ui32(bn_env, P.y, 0) == 0) {
+    //     cgbn_set_ui32(bn_env, R.x, 0);
+    //     cgbn_set_ui32(bn_env, R.y, 0);
+    //     return;
+    // }
+
+    cgbn_add(bn_env, t1, P.y, P.y); 
+    cgbn_modular_inverse(bn_env, t1, t1, Pmod); // t1 = 1/(2y)
+
+    cgbn_set_ui32(bn_env, t_three, 3);
+
+    // Convert to Montgomery space
+    np0 = cgbn_bn2mont(bn_env, x, P.x, Pmod);
+    cgbn_bn2mont(bn_env, t1, t1, Pmod);
+    cgbn_bn2mont(bn_env, y, P.y, Pmod);
+    cgbn_bn2mont(bn_env, t_three, t_three, Pmod);
+    cgbn_bn2mont(bn_env, a, a, Pmod);
+
+    // Compute s = (3x^2) + a / (2y) mod Pmod
+    cgbn_mont_sqr(bn_env, s, x, Pmod, np0);
+    cgbn_mont_mul(bn_env, s, s, t_three, Pmod, np0);
+    cgbn_add(bn_env, s, s, a);
+    cgbn_mont_mul(bn_env, s, s, t1, Pmod, np0);
+
+    env192_t::cgbn_t s_sq, t3, x3, y3;
+    
+    cgbn_mont_sqr(bn_env, s_sq, s, Pmod, np0); // s^2 mod Pmod // tested
+
+    cgbn_add(bn_env, t3, x, x); // x1 + x2
+
+    if (cgbn_sub(bn_env, x3, s_sq, t3)) // x3 = s^2 - x1 - x2 // mod Pmod
     {
-        R = add_points(bn_env, R, Q, Pmod);
+        cgbn_sub(bn_env, x3, t3, s_sq);
+        cgbn_sub(bn_env, x3, Pmod, x3);
     }
 
-    return R;
+    if (cgbn_sub(bn_env, t3, x, x3)) // t3 = x1 - x3 // mod Pmod
+    {
+        cgbn_sub(bn_env, t3, x3, x);
+        cgbn_sub(bn_env, t3, Pmod, t3);
+    }
+
+    cgbn_mont_mul(bn_env, t3, t3, s, Pmod, np0);
+
+    if (cgbn_sub(bn_env, y3, t3, y))
+    {
+        cgbn_sub(bn_env, y3, y, t3);
+        cgbn_sub(bn_env, y3, Pmod, y3);
+    }
+
+    cgbn_mont2bn(bn_env, x3, x3, Pmod, np0);
+    cgbn_mont2bn(bn_env, y3, y3, Pmod, np0);
+
+    // cgbn_sub(bn_env, x3, s_sq, t1);
+
+    return dev_ECC_192_point{x3, y3};
 }
 
 #ifndef UNIT_TESTING
--- tests/test_add_points_ECC79p.cu
+++ tests/test_add_points_ECC79p.cu
@@ -30,6 +30,30 @@ __global__ void kernel_test(cgbn_error_report_t *report, ECC_192_point *points, 
     cgbn_store(bn192_env, &(points[0].y), result.y);
 }
 
+// minimal kernel to test double point
+__global__ void kernel_test_double(cgbn_error_report_t *report, ECC_192_point *points, EC_parameters *parameters)
+{
+    context_t bn_context(cgbn_report_monitor, report, 1); // construct a context
+    env192_t bn192_env(bn_context.env<env192_t>());
+
+    env192_t::cgbn_t Pmod, a;
+    dev_ECC_192_point P0, P1;
+
+    cgbn_load(bn192_env, P0.x, &(points[0].x));
+    cgbn_load(bn192_env, P0.y, &(points[0].y));
+
+    cgbn_load(bn192_env, P1.x, &(points[1].x));
+    cgbn_load(bn192_env, P1.y, &(points[1].y));
+
+    cgbn_load(bn192_env, Pmod, &(parameters->Pmod));
+    cgbn_load(bn192_env, a, &(parameters->a));
+
+    dev_ECC_192_point result = double_point(bn192_env, P0, P0, Pmod, a);
+
+    cgbn_store(bn192_env, &(points[0].x), result.x);
+    cgbn_store(bn192_env, &(points[0].y), result.y);
+}
+
 TEST_CASE("ECC_79p add points [1]")
 {
     ECC_192_point *points, *gpuPoints;
@@ -108,6 +132,83 @@ TEST_CASE("ECC_79p add points [1]")
     REQUIRE(points[0].y._limbs[0] == 0x7328d137);
     REQUIRE(points[0].y._limbs[1] == 0x489dff15);
     REQUIRE(points[0].y._limbs[2] == 0x000027b6);
+    REQUIRE(points[0].y._limbs[3] == 0x0);
+    REQUIRE(points[0].y._limbs[4] == 0x0);
+    REQUIRE(points[0].y._limbs[5] == 0x0);
+
+    free(points);
+}
+
+TEST_CASE("ECC_79p double point [1]")
+{
+    ECC_192_point *points, *gpuPoints;
+    EC_parameters *parameters, *gpuParameters;
+    cgbn_error_report_t *report;
+
+    points = (ECC_192_point *)malloc(sizeof(ECC_192_point) * 2);
+    parameters = (EC_parameters *)malloc(sizeof(EC_parameters));
+
+    // POINT A
+    points[0].x._limbs[0] = 0x8475057d;
+    points[0].x._limbs[1] = 0x4b201c20;
+    points[0].x._limbs[2] = 0x0000315d;
+    points[0].x._limbs[3] = 0x0;
+    points[0].x._limbs[4] = 0x0;
+    points[0].x._limbs[5] = 0x0;
+
+    points[0].y._limbs[0] = 0x0252450a;
+    points[0].y._limbs[1] = 0x3df5ab37;
+    points[0].y._limbs[2] = 0x0000035f;
+    points[0].y._limbs[3] = 0x0;
+    points[0].y._limbs[4] = 0x0;
+    points[0].y._limbs[5] = 0x0;
+
+    parameters->Pmod._limbs[0] = 0xca899cf5;
+    parameters->Pmod._limbs[1] = 0x5177412a;
+    parameters->Pmod._limbs[2] = 0x000062ce;
+    parameters->Pmod._limbs[3] = 0x0;
+    parameters->Pmod._limbs[4] = 0x0;
+    parameters->Pmod._limbs[5] = 0x0;
+
+    parameters->a._limbs[0] = 0xbc45733c;
+    parameters->a._limbs[1] = 0x5e6dddb1;
+    parameters->a._limbs[2] = 0x000039c9;
+    parameters->a._limbs[3] = 0x0;
+    parameters->a._limbs[4] = 0x0;
+    parameters->a._limbs[5] = 0x0;
+
+    cudaCheckError(cudaSetDevice(0));
+
+    cudaCheckError(cudaMalloc((void **)&gpuPoints, sizeof(ECC_192_point) * 2));
+    cudaCheckError(cudaMemcpy(gpuPoints, points, sizeof(ECC_192_point) * 2, cudaMemcpyHostToDevice));
+
+    cudaCheckError(cudaMalloc((void **)&gpuParameters, sizeof(EC_parameters)));
+    cudaCheckError(cudaMemcpy(gpuParameters, parameters, sizeof(EC_parameters), cudaMemcpyHostToDevice));
+
+    cudaCheckError(cgbn_error_report_alloc(&report));
+
+    kernel_test_double<<<(INSTANCES + TPI - 1) / TPI, 128>>>(report, gpuPoints, gpuParameters);
+
+    cudaCheckError(cudaDeviceSynchronize());
+
+    CGBN_CHECK(report);
+
+    cudaCheckError(cudaMemcpy(points, gpuPoints, sizeof(ECC_192_point) * 2, cudaMemcpyDeviceToHost));
+
+    cudaCheckError(cudaFree(gpuPoints));
+    cudaCheckError(cgbn_error_report_free(report));
+
+    // ASSERT R = P + Q
+    REQUIRE(points[0].x._limbs[0] == 0x47e3e095);
+    REQUIRE(points[0].x._limbs[1] == 0x3e221adb);
+    REQUIRE(points[0].x._limbs[2] == 0x00004659);
+    REQUIRE(points[0].x._limbs[3] == 0x0);
+    REQUIRE(points[0].x._limbs[4] == 0x0);
+    REQUIRE(points[0].x._limbs[5] == 0x0);
+
+    REQUIRE(points[0].y._limbs[0] == 0x0725a4e3);
+    REQUIRE(points[0].y._limbs[1] == 0x42bee392);
+    REQUIRE(points[0].y._limbs[2] == 0x000059d6);
     REQUIRE(points[0].y._limbs[3] == 0x0);
     REQUIRE(points[0].y._limbs[4] == 0x0);
     REQUIRE(points[0].y._limbs[5] == 0x0);
