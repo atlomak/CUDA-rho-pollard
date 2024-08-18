6a7dfb92d494a3163353460ecadd19b2272cb1e0
atlomak <andrzej.tlomak01@gmail.com>
2024-08-18 21:52:52 +0200

    refactor: avoid reading wrong result by adding additional buffer

--- src/rho_pollard_kernel.cu
+++ src/rho_pollard_kernel.cu
@@ -6,7 +6,7 @@
 
 #define PRECOMPUTED_POINTS 1024
 
-void zeromq_client(int32_t instances, volatile int32_t *warp_flags, volatile int32_t *stop_flag, EC_point *h_starting);
+void zeromq_client(int32_t instances, volatile int32_t *warp_flags, volatile int32_t *stop_flag, EC_point *h_starting, EC_point *h_result);
 
 __shared__ EC_point SMEMprecomputed[PRECOMPUTED_POINTS];
 
@@ -22,6 +22,7 @@ __device__ uint32_t map_to_index(env192_t &bn_env, const dev_EC_point &P, const 
 typedef struct
 {
     EC_point *starting;
+    EC_point *result;
     EC_point *precomputed;
     EC_parameters *parameters;
     int32_t instances;
@@ -67,6 +68,7 @@ __global__ void rho_pollard(cgbn_error_report_t *report, volatile rho_pollard_ar
 
     cgbn_load(bn192_env, W.x, &(args.starting[instance].x));
     cgbn_load(bn192_env, W.y, &(args.starting[instance].y));
+    cgbn_load(bn192_env, W.seed, &(args.starting[instance].seed));
 
     cgbn_load(bn192_env, params.Pmod, &(args.parameters->Pmod));
     cgbn_load(bn192_env, params.a, &(args.parameters->a));
@@ -75,7 +77,7 @@ __global__ void rho_pollard(cgbn_error_report_t *report, volatile rho_pollard_ar
 
     while (*args.stop_flag == 0)
     {
-        while (!is_distinguish(bn192_env, W, args.parameters->zeros_count))
+        while (!is_distinguish(bn192_env, W, args.parameters->zeros_count) && *args.stop_flag == 0)
         {
             uint32_t precomp_index = map_to_index(bn192_env, W, mask);
             cgbn_load(bn192_env, R.x, &(SMEMprecomputed[precomp_index].x));
@@ -85,47 +87,49 @@ __global__ void rho_pollard(cgbn_error_report_t *report, volatile rho_pollard_ar
 
         // synchronization with CPU
         __syncwarp();
-        cgbn_store(bn192_env, &(args.starting[instance].x), W.x);
-        cgbn_store(bn192_env, &(args.starting[instance].y), W.y);
+        cgbn_store(bn192_env, &(args.result[instance].x), W.x);
+        cgbn_store(bn192_env, &(args.result[instance].y), W.y);
+        cgbn_store(bn192_env, &(args.result[instance].seed), W.seed);
+        __threadfence_system();
         if (thread_id % 32 == 0)
         {
-            while (atomicCAS(&args.warp_flags[warp_id], 0, 1))
-                ;
+            args.warp_flags[warp_id] = 1;
+            __threadfence_system();
+            // while (atomicCAS(&args.warp_flags[warp_id], 0, 1))
+            //     ;
             // printf("Warp %d found 8 distinguish points! Flag: %d\n", warp_id, *args.stop_flag);
         }
         __syncwarp();
         if (thread_id % 32 == 0)
         {
-            while (!atomicCAS(&args.warp_flags[warp_id], 0, 0)) // While CPU set flat to 0, warp can continue
+            while (args.warp_flags[warp_id] != 0) // While CPU set flat to 0, warp can continue
                 ;
+            __threadfence_system();
             // printf("Warp %d continue\n", warp_id);
         }
         __syncwarp();
         cgbn_load(bn192_env, W.x, &(args.starting[instance].x));
         cgbn_load(bn192_env, W.y, &(args.starting[instance].y));
+        cgbn_load(bn192_env, W.seed, &(args.starting[instance].seed));
+        __syncwarp();
     }
-    __syncwarp();
-    printf("Warp %d stopped. Flag %d\n", warp_id, *args.stop_flag);
+    // printf("Warp %d stopped. Flag %d\n", warp_id, *args.stop_flag);
 }
 
 extern "C" {
 void run_rho_pollard(EC_point *startingPts, uint32_t instances, EC_point *precomputed_points, EC_parameters *parameters)
 {
     printf("Starting rho pollard: zeroes count %d", parameters->zeros_count);
-    EC_point *gpu_starting;
-    EC_point *h_starting;
-
-    EC_point *gpu_precomputed;
-    EC_parameters *gpu_params;
     cgbn_error_report_t *report;
 
     cudaSetDevice(0);
     cudaCheckErrors("Failed to set device");
 
+    // STARTING POINTS
+    EC_point *gpu_starting;
+    EC_point *h_starting;
     cudaHostAlloc((void **)&h_starting, sizeof(EC_point) * instances, cudaHostAllocMapped);
     cudaCheckErrors("Failed to allocate memory for starting points");
-
-    memcpy(h_starting, startingPts, sizeof(EC_point) * instances);
 
     cudaHostGetDevicePointer(&gpu_starting, h_starting, 0);
     cudaCheckErrors("Failed to get device pointer for starting points");
@@ -133,23 +137,38 @@ void run_rho_pollard(EC_point *startingPts, uint32_t instances, EC_point *precom
     cudaMemcpy(gpu_starting, startingPts, sizeof(EC_point) * instances, cudaMemcpyHostToDevice);
     cudaCheckErrors("Failed to copy starting points to device");
 
+    // RESULT POINTS
+    EC_point *gpu_result;
+    EC_point *h_result;
+    cudaHostAlloc((void **)&h_result, sizeof(EC_point) * instances, cudaHostAllocMapped);
+    cudaCheckErrors("Failed to allocate memory for result points");
+
+    cudaHostGetDevicePointer(&gpu_result, h_result, 0);
+    cudaCheckErrors("Failed to get device pointer for result points");
+
+    // PRECOMPUTED POINTS
+    EC_point *gpu_precomputed;
     cudaMalloc((void **)&gpu_precomputed, sizeof(EC_point) * PRECOMPUTED_POINTS);
     cudaCheckErrors("Failed to allocate memory for precomputed points");
 
     cudaMemcpy(gpu_precomputed, precomputed_points, sizeof(EC_point) * PRECOMPUTED_POINTS, cudaMemcpyHostToDevice);
     cudaCheckErrors("Failed to copy precomputed points to device");
 
+    // PARAMETERS
+    EC_parameters *gpu_params;
     cudaMalloc((void **)&gpu_params, sizeof(EC_parameters));
     cudaCheckErrors("Failed to allocate memory for parameters");
 
     cudaMemcpy(gpu_params, parameters, sizeof(EC_parameters), cudaMemcpyHostToDevice);
     cudaCheckErrors("Failed to copy parameters to device");
 
+    // STOP FLAG
     int32_t *h_stop_flag;
     int32_t *d_stop_flag;
     cudaHostAlloc((void **)&h_stop_flag, sizeof(int32_t), cudaHostAllocMapped);
     cudaHostGetDevicePointer(&d_stop_flag, h_stop_flag, 0);
 
+    // WARP FLAGS
     int32_t *h_warp_flags;
     int32_t *d_warp_flags;
     cudaHostAlloc((void **)&h_warp_flags, sizeof(int32_t) * (instances + 7 / 8), cudaHostAllocMapped);
@@ -161,6 +180,7 @@ void run_rho_pollard(EC_point *startingPts, uint32_t instances, EC_point *precom
 
     rho_pollard_args args;
     args.starting = gpu_starting;
+    args.result = gpu_result;
     args.precomputed = gpu_precomputed;
     args.parameters = gpu_params;
     args.instances = instances;
@@ -172,7 +192,7 @@ void run_rho_pollard(EC_point *startingPts, uint32_t instances, EC_point *precom
 
     // 512 threads per block (128 CGBN instances)
     rho_pollard<<<(instances + 127) / 128, 512>>>(report, args);
-    zeromq_client(instances, h_warp_flags, h_stop_flag, h_starting);
+    zeromq_client(instances, h_warp_flags, h_stop_flag, h_starting, h_result);
 
     cudaDeviceSynchronize();
     cudaCheckErrors("Kernel failed");
@@ -183,8 +203,11 @@ void run_rho_pollard(EC_point *startingPts, uint32_t instances, EC_point *precom
     cudaMemcpy(startingPts, gpu_starting, sizeof(EC_point) * instances, cudaMemcpyDeviceToHost);
     cudaCheckErrors("Failed to copy starting points to host");
 
-    cudaFree(gpu_starting);
+    cudaFreeHost(gpu_starting);
     cudaCheckErrors("Failed to free memory for starting points");
+
+    cudaFreeHost(gpu_result);
+    cudaCheckErrors("Failed to free memory for result points");
 
     cudaFree(gpu_precomputed);
     cudaCheckErrors("Failed to free memory for precomputed points");
@@ -197,7 +220,7 @@ void run_rho_pollard(EC_point *startingPts, uint32_t instances, EC_point *precom
 }
 }
 
-void zeromq_client(int32_t instances, volatile int32_t *warp_flags, volatile int32_t *stop_flag, EC_point *h_starting)
+void zeromq_client(int32_t instances, volatile int32_t *warp_flags, volatile int32_t *stop_flag, EC_point *h_starting, EC_point *h_result)
 {
     int32_t warps = (instances + 7) / 8;
 
@@ -209,27 +232,35 @@ void zeromq_client(int32_t instances, volatile int32_t *warp_flags, volatile int
     new_starting_points = (EC_point *)malloc(sizeof(EC_point) * 8);
     distinguish_points = (EC_point *)malloc(sizeof(EC_point) * 8);
 
-    int32_t counter = 0;
-    while (counter < 10000)
+    while (*stop_flag == 0)
     {
         for (int i = 0; i < warps; i++)
         {
             if (warp_flags[i] == 1)
             {
                 printf("CPU: Warp %d found 8 distinguish points\n", i);
-                counter++;
-                memcpy(distinguish_points, h_starting + i * 8, sizeof(EC_point) * 8);
+                memcpy(distinguish_points, h_result + i * 8, sizeof(EC_point) * 8);
                 zmq_send(requester, distinguish_points, sizeof(EC_point) * 8, 0);
 
                 printf("CPU: Requesting new starting points\n");
-                zmq_recv(requester, new_starting_points, sizeof(EC_point) * 8, 0);
+                int msg_size = zmq_recv(requester, new_starting_points, sizeof(EC_point) * 8, 0);
+                if (msg_size == 0)
+                {
+                    printf("CPU: Received stop signal\n");
+                    *stop_flag = 1;
+                    break;
+                }
                 printf("CPU: Received new starting points\n");
                 memcpy(h_starting + i * 8, new_starting_points, sizeof(EC_point) * 8);
                 warp_flags[i] = 0;
             }
         }
     }
-    *stop_flag = 1;
+
+    for (int i = 0; i < warps; i++)
+    {
+        warp_flags[i] = 0;
+    }
 
     zmq_close(requester);
     zmq_ctx_destroy(context);
